5190c3073c45ce551e0e01c80408f3f8f5b4c7b7
Amit Patel <amitptl.in@gmail.com>
2021-03-27 02:17:35 +0400

    Customizing the Pipeline Options and passing the runtime args for source and sink file path

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Beam-Tutorial
+
+
+
+
+#Run the application to calculate the marks of each candidate
+```maven
+mvn compile exec:java -Dexec.mainClass=com.amiown.beam.tutorial.TotalScoreComputation -Dexec.args="--outputFile=src/main/resources/sink/student_total_scores.csv --inputFile=src/main/resources/source/student_scores.csv"
+```
--- src/main/java/com/amiown/beam/tutorial/TotalScoreComputation.java
+++ src/main/java/com/amiown/beam/tutorial/TotalScoreComputation.java
@@ -3,8 +3,7 @@ package com.amiown.beam.tutorial;
 import org.apache.beam.repackaged.core.org.apache.commons.lang3.StringUtils;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.options.PipelineOptions;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.*;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.KV;
@@ -12,15 +11,32 @@ import org.apache.beam.sdk.values.KV;
 public class TotalScoreComputation {
     private static final String CSV_HEADER = "ID,Name,Physics,Chemistry,Math,English,Biology,History";
 
+    public interface TotalScoreComputationOptions extends  PipelineOptions {
+
+        @Description("Path of the file to read from")
+        @Default.String("src/main/resources/source/student_scores.csv")
+        String getInputFile();
+
+        void setInputFile(String value);
+
+        @Description("Path of the file to write to")
+      //  @Default.String("src/main/resources/source/sink/student_total_scores.csv")
+        @Validation.Required
+        String getOutputFile();
+
+        void setOutputFile(String value);
+    }
+
     public static void main(String... s) {
-        PipelineOptions options = PipelineOptionsFactory.create();
+        TotalScoreComputationOptions options = PipelineOptionsFactory.fromArgs(s)
+                .withValidation().as(TotalScoreComputationOptions.class);
         Pipeline pipeline = Pipeline.create(options);
 
-        pipeline.apply(TextIO.read().from("src/main/resources/source/student_scores.csv"))
+        pipeline.apply(TextIO.read().from(options.getInputFile()))
                 .apply(ParDo.of(new FilterHeaderFn(CSV_HEADER)))
                 .apply(ParDo.of(new ComputeTotalScoreFn()))
                 .apply(ParDo.of(new ConvertToStringFn()))
-                .apply(TextIO.write().to("src/main/resources/sink/student_total_scores.csv")
+                .apply(TextIO.write().to(options.getOutputFile())
                 //add header in output file
                         .withHeader("Name,Total")
                 //make sure to write into a single file
